c6cd2901c4c14bd271123c4e98c31ae3047d56a2
Igbescobar <igbescobar@gmail.com>
2026-04-13 16:54:05 +0200

    initial implementation of response

---
+++ inc/HttpResponse.hpp
@@ -0,0 +1,30 @@
+#ifndef HTTPRESPONSE_HPP
+#define HTTPRESPONSE_HPP
+
+#include "IResponse.hpp"
+#include <map>
+#include <sstream>
+#include <string>
+
+class HttpResponse : public IResponse {
+private:
+  std::string version;
+  int statusCode;
+  std::string statusMessage;
+  std::map<std::string, std::string> headers;
+  std::string body;
+
+  void setStatusMessage(int code);
+
+public:
+  HttpResponse();
+  ~HttpResponse();
+
+  void setStatusCode(int code);
+  void setHeader(const std::string &key, const std::string &value);
+  void setBody(const std::string &body);
+
+  std::string toString() const;
+};
+
+#endif
---
+++ src/HttpResponse.cpp
@@ -0,0 +1,49 @@
+#include "../inc/HttpResponse.hpp"
+
+HttpResponse::HttpResponse()
+    : version("HTTP/1.1"), statusCode(200), statusMessage("OK") {}
+
+HttpResponse::~HttpResponse() {}
+
+void HttpResponse::setStatusMessage(int code) {
+  switch (code) {
+  case 200:
+    this->statusMessage = "OK";
+    break;
+  case 201:
+    this->statusMessage = "Created";
+    break;
+  case 400:
+    this->statusMessage = "Bad Request";
+    break;
+  case 404:
+    this->statusMessage = "Not Found";
+    break;
+  case 405:
+    this->statusMessage = "Method Not Allowed";
+    break;
+  case 500:
+    this->statusMessage = "Internal Server Error";
+    break;
+  default:
+    this->statusMessage = "Unknown";
+    break;
+  }
+}
+
+void HttpResponse::setStatusCode(int code) {
+  this->statusCode = code;
+  setStatusMessage(code);
+}
+
+void HttpResponse::setHeader(const std::string &key, const std::string &value) {
+  this->headers[key] = value;
+}
+
+void HttpResponse::setBody(const std::string &body) {
+  this->body = body;
+
+  std::stringstream ss;
+  ss << this->body.length();
+  setHeader("Content-Length", ss.str());
+}
