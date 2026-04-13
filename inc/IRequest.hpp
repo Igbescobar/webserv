782a959387c58d96b449b11cffca86d59aadc1f3
Igbescobar <igbescobar@gmail.com>
2026-04-13 13:01:57 +0200

    finish implementing interfaces

---
+++ inc/IRequest.hpp
@@ -0,0 +1,21 @@
+#ifndef IREQUEST_HPP
+#define IREQUEST_HPP
+
+#include <map>
+#include <string>
+
+class IRequest {
+
+  virtual ~IRequest() {}
+
+  virtual std::string getMethod() const = 0;
+  virtual std::string getUri() const = 0;
+  virtual std::string getVersion() const = 0;
+
+  virtual std::string getHeader(const std::string &key) const = 0;
+  virtual std::map<std::string, std::string> getHeaders() const = 0;
+
+  virtual std::string getBody() const = 0;
+};
+
+#endif
---
+++ inc/IResponse.hpp
@@ -0,0 +1,17 @@
+#ifndef IRESPONSE_HPP
+#define IRESPONSE_HPP
+
+#include <string>
+
+class IResponse {
+public:
+  virtual ~IResponse() {}
+
+  virtual void setStatusCode(int code) = 0;
+  virtual void setHeader(const std::string &key, const std::string &value) = 0;
+  virtual void setBody(const std::string &body) = 0;
+
+  virtual std::string toString() const = 0;
+};
+
+#endif
