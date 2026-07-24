7ddb642c55f24ddb83e7f518f95c1afe3c90fe94
marcos <marcoslopez1305@gmail.com>
2026-07-24 21:47:49 +0200

    remove everything

--- cgi-scripts/hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-import sys
-
-#print("Content-Type: text/plain")
-#print("")
-print("METHOD: " + os.environ.get("REQUEST_METHOD", ""))
-print("QUERY: " + os.environ.get("QUERY_STRING", ""))
-print("SCRIPT: " + os.environ.get("SCRIPT_FILENAME", ""))
--- tests/concurrent_slow_requests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..100}; do
-	./slow_request.sh &
-done
-
-wait
